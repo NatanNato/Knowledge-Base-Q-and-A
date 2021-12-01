bb52cc3950a50cea205a475bdf1a7810298d3695
NatanNato <itismytime1221@gmail.com>
2021-12-01 08:12:08 -0800

    Login and registration page

---
+++ knowledge_base_Q_and_A/login/jScript.js
@@ -0,0 +1,15 @@
+/*
+var log=document.getElementById('login');
+var reg=document.getElementById('register');
+function register()
+{
+    log.style.display = '';
+    reg.style.display = 'none';
+}
+function login()
+{
+    log.style.display = 'none';
+    reg.style.display = '';
+}
+*/
+
